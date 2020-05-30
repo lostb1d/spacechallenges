edcc1a944bb2c5281fae870d9386fecc4880e429
lostb1d <mylife.dinesh08@gmail.com>
2020-05-30 16:24:09 +0545

    views.py updated

--- forestChangeDetection/views.py
+++ forestChangeDetection/views.py
@@ -1,6 +1,12 @@
 from django.shortcuts import render, HttpResponse
-
+import ee
+ee.Initialize()
+import os
 # Create your views here.
 
 def index(request):
+    
     return render(request,'index.html')
+
+def index_calculation(a,b):
+    return a.subtract(b).divide(a.add(b))
