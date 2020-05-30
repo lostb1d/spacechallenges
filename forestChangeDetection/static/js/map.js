b5a294f1a8f818a31406769236550b220968f33a
kaditya97 <kaditya9711@gmail.com>
2020-05-30 19:49:11 +0545

    layout update

--- forestChangeDetection/static/js/map.js
+++ forestChangeDetection/static/js/map.js
@@ -9,21 +9,62 @@ L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_toke
     accessToken: 'pk.eyJ1IjoibG9zdGIxIiwiYSI6ImNqaTBjcGd4bjE2cGMza3M2MWEzcTRwd3gifQ.Ps6yKHol2bmEPndMSeYKKw'
 }).addTo(mymap);
 
-// L.geoJSON(nepal_data).addTo(mymap);
+function getData(){
+    var inputNode = document.createElement('input');
+        inputNode.setAttribute('type','file');
+        inputNode.setAttribute('id','leaflet-draw-shapefile-selector');
+        inputNode.setAttribute('accept','.geojson');
 
-function check(data){
-    
-    
-    // $.getJSON(data.value, function(data1){
-    //     L.geoJSON(data1).addTo(mymap);
-    // })
-    
+        inputNode.addEventListener("change", function(e){
+          var files = inputNode.files;
+          var file = files[0];
+          var reader = new FileReader();
+          reader.onload = function() {
+            var geo = omnivore.geojson(reader.result)
+            .on('ready',function(){
+                mymap.fitBounds(geo.getBounds());
+              })
+            .addTo(mymap);
+            };
+          reader.readAsDataURL(file);
+        });
+        inputNode.click();
   }
-  document.getElementById('input-file-now').addEventListener('change', function() { 
-    
-  var fr=new FileReader(); 
-  fr.onload=function(){ 
-      document.getElementById('output').textContent=fr.result; 
-  } 
-  fr.readAsText(this.files[0]); 
-}) 
+var drawnItems;
+function draw(){
+    if(drawnItems==undefined){
+        drawnItems = new L.FeatureGroup();
+        mymap.addLayer(drawnItems);
+        var drawControl = new L.Control.Draw({
+            // position: 'topright',
+            draw: {
+                polygon:  {
+                    shapeOptions: {
+                     color: 'red'
+                    },
+                   },
+                polyline: false,
+                rect: {
+                shapeOptions: {
+                color: 'green'
+                },
+                },
+                circle: false,
+                circlemarker: false,
+                marker: false
+            },
+                edit: {
+                    featureGroup: drawnItems,
+                    edit: false
+                }
+            });
+        mymap.addControl(drawControl);
+        mymap.on('draw:created', function (e) {
+                    drawnItems.addLayer(e.layer);
+                });
+        mymap.on(L.Draw.Event.CREATED, function (e) {
+            var layer = e.layer;
+        mymap.addLayer(layer);
+        });
+    }else{return;}
+}
